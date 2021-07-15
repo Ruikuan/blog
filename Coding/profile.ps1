0156c97a559ac05a3b2973a9c5f9af317ab327da
Ruikuan <liruikuan@outlook.com>
2021-07-15 15:29:54 +0800

    Create profile.ps1

---
+++ Coding/profile.ps1
@@ -0,0 +1,7 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme marcduiker
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
